9fb67384f0aedf394bbcc20613d761b73609fa5d
viktor.hofer@outlook.com <viktor.hofer@outlook.com>
2015-05-15 14:35:44 +0200

    Updated assembly infos, included nuspec files, updated gitignore

--- TheTVDBSharp.Portable/Properties/AssemblyInfo.cs
+++ TheTVDBSharp.Portable/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TheTVDBSharp.Portable")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TheTVDBSharp Portable")]
+[assembly: AssemblyDescription("TheTVDB API access library with huge support for all current plattforms: Win8+WP8+NET45+WP81")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TheTVDBSharp.Portable")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Viktor Hofer")]
+[assembly: AssemblyProduct("TheTVDBSharp Portable")]
+[assembly: AssemblyCopyright("Copyright © Viktor Hofer 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -25,6 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1")]
--- TheTVDBSharp.UAP/Properties/AssemblyInfo.cs
+++ TheTVDBSharp.UAP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TheTVDBSharp.UAP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TheTVDBSharp UAP")]
+[assembly: AssemblyDescription("TheTVDB API access library with support for all flavours of Windows 10")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TheTVDBSharp.UAP")]
+[assembly: AssemblyCompany("Viktor Hofer")]
+[assembly: AssemblyProduct("TheTVDBSharp UAP")]
 [assembly: AssemblyCopyright("Copyright © Viktor Hofer 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1")]
 [assembly: ComVisible(false)]
